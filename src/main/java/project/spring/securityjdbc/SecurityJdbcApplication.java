999c6a070c1639c6b5cd2a13d1334d3fd707e8c8
Nicolas <aponcenicolas@gmail.com>
2020-11-16 17:40:03 -0500

    user security with jdbc-api

---
+++ src/main/java/project/spring/securityjdbc/SecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package project.spring.securityjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJdbcApplication.class, args);
+	}
+
+}
